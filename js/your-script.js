fbcd40dc9eff83f7f185656130be364ba8696f8b
samwilson92 <35769137+samwilson92@users.noreply.github.com>
2023-11-08 15:57:43 +0000

    Update your-script.js

--- js/your-script.js
+++ js/your-script.js
@@ -1,51 +1,84 @@
-document.getElementById('file-input').addEventListener('change', handleFileSelect, false);
+// Elements from the DOM
+const fileInput = document.getElementById('file-input');
+const saveButton = document.getElementById('save-button');
 
+// Global map variables
+let map;
+let heatmapLayer;
+let gpxPoints = [];
+
+// Event listener for file input change
+fileInput.addEventListener('change', handleFileSelect, false);
+
+// Initially hide the save button
+saveButton.style.display = 'none';
+
+// Handle file selection and read the file
 function handleFileSelect(event) {
-    const reader = new FileReader();
-    reader.onload = function(fileEvent) {
-        const contents = fileEvent.target.result;
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
         
-        // Parse the GPX data
-        const gpx = new DOMParser().parseFromString(contents, "text/xml");
-        const tracks = gpx.getElementsByTagName("trkpt");
-        const points = [];
-        let lat, lon;
-
-        for (let i = 0; i < tracks.length; i++) {
-            lat = parseFloat(tracks[i].getAttribute("lat"));
-            lon = parseFloat(tracks[i].getAttribute("lon"));
-            points.push([lat, lon]);
-        }
-
-        if (points.length === 0) {
-            console.error('No points found in the GPX file.');
-            return;
-        }
-
-        // Initialize the map
-        const map = L.map('map');
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 18
-        }).addTo(map);
-
-        // Calculate bounds and padding
-        const bounds = L.latLngBounds(points);
-        const padding = bounds.pad(0.1); // Adds 10% padding
-
-        // Fit the map bounds to the heatmap with padding
-        map.fitBounds(padding);
-
-        // Create and display the heatmap
-        L.heatLayer(points, {
-            radius: 25,
-            blur: 15,
-            maxZoom: 17,
-        }).addTo(map);
-    };
-
-    // Read the file as text
-    reader.readAsText(event.target.files[0]);
+        reader.onload = function(e) {
+            parseGPX(e.target.result);
+            initializeMap();
+            document.getElementById('map-preview').style.display = 'block';
+        };
+        
+        reader.readAsText(file);
+    }
 }
 
-// Leaflet's 'fitBounds' method automatically adjusts the view to contain the bounding box with padding
-// This makes additional manual padding calculation unnecessary.
+// Parse the GPX data
+function parseGPX(gpxContents) {
+    const parser = new DOMParser();
+    const gpx = parser.parseFromString(gpxContents, "application/xml");
+    const trkpts = gpx.querySelectorAll("trkpt");
+    
+    gpxPoints = []; // Clear previous points
+
+    trkpts.forEach(trkpt => {
+        const lat = parseFloat(trkpt.getAttribute("lat"));
+        const lon = parseFloat(trkpt.getAttribute("lon"));
+        gpxPoints.push([lat, lon]);
+    });
+
+    if (gpxPoints.length === 0) {
+        console.error('No points found in the GPX file.');
+        return;
+    }
+
+    // Enable the save button once the GPX is parsed
+    saveButton.style.display = 'inline-block';
+}
+
+// Initialize the map
+function initializeMap() {
+    if (!map) {
+        map = L.map('map').setView([51.505, -0.09], 13);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 18,
+            attribution: '© OpenStreetMap contributors'
+        }).addTo(map);
+    }
+
+    // Fit the map bounds to the GPX points with padding
+    const bounds = L.latLngBounds(gpxPoints).pad(0.1);
+    map.fitBounds(bounds);
+}
+
+// Event listener for the save button to create heatmap
+saveButton.addEventListener('click', createHeatmap, false);
+
+// Create and add the heatmap layer
+function createHeatmap() {
+    if (heatmapLayer) {
+        heatmapLayer.remove(); // Remove the existing heatmap layer if present
+    }
+    
+    heatmapLayer = L.heatLayer(gpxPoints, {
+        radius: 25,
+        blur: 15,
+        maxZoom: 17,
+    }).addTo(map);
+}
