8619d26bd269d1740c9f038a55dbbcc23f3eea5f
samwilson92 <35769137+samwilson92@users.noreply.github.com>
2023-11-15 15:45:10 +0000

    Update your-script.js

--- js/your-script.js
+++ js/your-script.js
@@ -5,7 +5,6 @@ import StadiaMaps from 'ol/source/StadiaMaps.js';
 import TileLayer from 'ol/layer/Tile.js';
 import View from 'ol/View.js';
 
-import {fromLonLat} from 'ol/proj.js';
 
 // Elements from the DOM
 const fileInput = document.getElementById('file-input');
@@ -76,3 +75,56 @@ function parseGPX(gpxContents) {
         return;
     }
 }
+
+// Declare map and heatmapLayer variables
+let map, heatmapLayer;
+
+// Initialize the map
+function initializeMap() {
+    if (!map) {
+        // Create a new OpenLayers map
+        map = new ol.Map({
+            target: 'map', // The DOM element to attach the map to
+            layers: [
+                // Add a Tile layer using OpenStreetMap source
+                new ol.layer.Tile({
+                    source: new ol.source.OSM()
+                })
+            ],
+            view: new ol.View({
+                center: ol.proj.fromLonLat([-0.09, 51.505]), // Map center in longitude and latitude
+                zoom: 13 // Initial zoom level
+            })
+        });
+    }
+
+    // Fit the map bounds to the GPX points with padding
+    const extent = ol.extent.boundingExtent(gpxPoints.map(p => ol.proj.fromLonLat([p[1], p[0]])));
+    ol.extent.applyTransform(extent, ol.proj.getTransform("EPSG:4326", "EPSG:3857"));
+    map.getView().fit(extent, { padding: [50, 50, 50, 50] }); // Add padding around the extent
+}
+
+// Create and add the heatmap layer
+function createHeatmap() {
+    if (heatmapLayer) {
+        map.removeLayer(heatmapLayer); // Remove the existing heatmap layer if present
+    }
+
+    // Transform GPX points to the format required by OpenLayers
+    const transformedPoints = gpxPoints.map(p => new ol.Feature(new ol.geom.Point(ol.proj.fromLonLat([p[1], p[0]]))));
+
+    // Create a vector source and add the transformed points
+    const vectorSource = new ol.source.Vector({
+        features: transformedPoints
+    });
+
+    // Create and add the heatmap layer
+    heatmapLayer = new ol.layer.Heatmap({
+        source: vectorSource,
+        radius: 25, // Equivalent to Leaflet's radius
+        blur: 15 // Equivalent to Leaflet's blur
+    });
+
+    map.addLayer(heatmapLayer);
+}
+
